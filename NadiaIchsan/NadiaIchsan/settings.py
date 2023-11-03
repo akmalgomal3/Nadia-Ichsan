6ead05af279854114a1e60149988417679247a3e
muhamad.akmal01 <muhamad.akmal01@ui.ac.id>
2023-11-03 21:00:27 +0700

    deploy

--- NadiaIchsan/NadiaIchsan/settings.py
+++ NadiaIchsan/NadiaIchsan/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-qz!g(_7cb_s1+!%5_#2iq#h+q)9rv)%b81j=-fv&pw3p1wf&fx'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ["https://nadiaikhsan.et.r.appspot.com/"]
 
 
 # Application definition
@@ -117,12 +117,12 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
 )
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
