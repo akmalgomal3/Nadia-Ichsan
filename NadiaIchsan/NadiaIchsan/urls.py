603c99a4105fc82f3618bafb70812a3bff7967b6
muhamad.akmal01 <muhamad.akmal01@ui.ac.id>
2023-11-03 23:16:22 +0700

    saddas

--- NadiaIchsan/NadiaIchsan/urls.py
+++ NadiaIchsan/NadiaIchsan/urls.py
@@ -15,10 +15,14 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.conf.urls import url, include
+from django.views.static import serve
+from NadiaIchsan.NadiaIchsan import settings
 
 import person.urls
 
 urlpatterns = [
+    url(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}),
     path('admin/', admin.site.urls),
     path('', include(person.urls)),
 ]
